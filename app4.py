c9d836344770090c28ac9e218f667f2ec4cc8fe5
Lucero Manrique Santos <107592025+smlucky12@users.noreply.github.com>
2026-05-25 23:45:06 -0500

    Update app4.py

--- app4.py
+++ app4.py
@@ -1,7 +1,13 @@
 # =========================================================
 # IMPORTS
 # =========================================================
-
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+import cv2
+import easyocr
 import streamlit as st
 import pandas as pd
 import numpy as np
